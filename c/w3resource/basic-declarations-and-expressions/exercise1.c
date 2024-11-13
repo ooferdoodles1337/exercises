744fc0eeca980a466be9379e6c01c0986fb1c01f
ooferdoodles <pantaisuyasri@gmail.com>
2024-11-13 16:04:03 +0900

    ye

---
+++ c/w3resource/basic-declarations-and-expressions/exercise1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int ex1()
+{
+    char name[] = "Pantai Suyasri";
+    char dob[] = "25/03/2006";
+    char mobile[] = "081-234-5678";
+
+    printf("Name   : %s\n", name);
+    printf("DOB    : %s\n", dob);
+    printf("Mobile : %s\n", mobile);
+    return 0;
+}
